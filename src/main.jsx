800045622193d9ebc4402d99e76478dd6ef59f1d
Gisela <giselamata27@gmail.com>
2024-03-04 12:11:56 -0500

    Importing all the pages

---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+// Importing the pages so the router will use these
+import App from './App';
+import ErrorPage from './pages/ErrorPage';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ContactPage from './pages/ContactPage';
+import PortfolioPage from './pages/PortfolioPage';
+import ProfilePage from './pages/ProfilePage';
