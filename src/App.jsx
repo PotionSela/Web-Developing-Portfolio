6d2025b5286963904a085514e12e486e3aaccfcd
Gisela <giselamata27@gmail.com>
2024-03-04 12:50:53 -0500

    Added Outlet and Nav componenets

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,14 @@
 // Bringing in the required import from 'react-router-dom'
 import { Outlet } from 'react-router-dom';
 import Nav from './components/Nav';
+
+function App() {
+    // The Outlet component will conditionally swap between the different pages
+    return (
+        <>
+        <Nav />
+        <Outlet />
+        </>
+    );
+}
+
