887ac22be9322d188effd5ca5ffdd68e855c5f1d
Gisela <giselamata27@gmail.com>
2024-03-04 13:01:47 -0500

    Added error page content

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,2 +1,16 @@
 import { useRouteError } from 'react-router-dom';
 
+export default function ErrorPage() {
+    const error = useRouteError();
+    console.error(error);
+
+    return (
+        <div id="error-page">
+            <h1>Oops!</h1>
+            <p>There seems to be an error! Try again!</p>
+            <p>
+                <i>{error.statusText || error.message}</i>
+            </p>
+        </div>
+    );
+}
