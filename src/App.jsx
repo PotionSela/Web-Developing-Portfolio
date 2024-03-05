364e9dcb36493d4b2dfa4d9e3da105b01c4a03cc
Gisela <giselamata27@gmail.com>
2024-03-05 13:06:30 -0500

    Fixed up set up for file

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,41 @@
-// Bringing in the required import from 'react-router-dom'
-import { Outlet } from 'react-router-dom';
-import Nav from './components/Nav';
+// Bringing in the required imports
+import { Routes, Route } from 'react-router-dom';
+import AboutPage from './pages/AboutPage';
+import ContactPage from './pages/ContactPage';
+import HomePage from './pages/HomePage';
+import PortfolioPage from './pages/PortfolioPage';
+import ProfilePage from './pages/ProfilePage';
 
-function App() {
-    // The Outlet component will conditionally swap between the different pages
+const App = () => {
+    // 
     return (
-        <>
-        <Nav />
-        <Outlet />
-        </>
-    );
-}
+        <Routes>
+            <Route
+                index
+                path='/'
+                element={<HomePage />}
+                />
+
+            <Route
+                path='/contact'
+                element={<ContactPage />}
+                />
+
+            <Route
+                path='/about'
+                element={<AboutPage />}
+                />
+
+            <Route
+                path='/portfolio'
+                element={<PortfolioPage />}
+                />
+
+            <Route
+                path='/profile'
+                element={<ProfilePage />}
+                />
+        </Routes>
+    )};
 
 export default App;
