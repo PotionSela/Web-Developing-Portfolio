b28b38819a5d918ee6ffdb3f857c3c2e7777cb6c
Gisela <giselamata27@gmail.com>
2024-03-05 12:57:05 -0500

    Fixed main imports and rendering router component

--- src/main.jsx
+++ src/main.jsx
@@ -1,54 +1,21 @@
+import React from 'react';
+// Importing the required imports from 'react-router-dom' to set up application routing behavior
+import { BrowserRouter as Router } from 'react-router-dom';
 // Importing the ReactDOM
 import ReactDOM from 'react-dom/client';
-// Importing the required imports from 'react-router-dom' to set up application routing behavior
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-
+// Importing the pages so the router will use these
+import App from './App';
 // Importing boostrap and index.css
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
-// Importing the pages so the router will use these
-import App from './App';
-import ErrorPage from './pages/ErrorPage';
-import HomePage from './pages/HomePage';
-import AboutPage from './pages/AboutPage';
-import ContactPage from './pages/ContactPage';
-import PortfolioPage from './pages/PortfolioPage';
-import ProfilePage from './pages/ProfilePage';
 
-// Router
-const router = createBrowserRouter([
-    // Define the accessible routes
-    {
-        path: '/',
-        element: <App />,
-        errorElement: <ErrorPage />,
-        children: [
-            {
-                index: true,
-                element: <HomePage />,
-            },
-            {
-                path: '/About',
-                element: <AboutPage />,
-            },
-            {
-                path: '/Contact',
-                element: <ContactPage />,
-            },
-            {
-                path: '/Portfolio',
-                element: <PortfolioPage />,
-            },
-            {
-                path: '/Profile',
-                element: <ProfilePage />,
-            },
-        ]
-    }
-]);
 
 // Rendering the RouterProvider component
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <RouterProvider router={router} />
+    <React.StrictMode>
+        <Router>
+            <App />
+        </Router>
+    </React.StrictMode>
 );
