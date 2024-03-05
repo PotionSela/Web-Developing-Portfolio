5b56bf8045e3eb43d2b7605b692b3ae76f918a94
Gisela <giselamata27@gmail.com>
2024-03-05 15:04:33 -0500

    Added imports of images

---
+++ src/utils/data.js
@@ -0,0 +1,7 @@
+// Import all the past projects that I want to display
+import employeeTracker from '../assets/Projects/Chaotic-Employee-Tracked-Down.png'
+import weatherApp from '../assets/Projects/Climate-Quest-Navigator.png'
+import dndStats from '../assets/Projects/DnD-Stat-Compiler.png'
+import friendZone from '../assets/Projects/Friendzone-API.png'
+import project2 from '../assets/Projects/Off-The-Beaten-Path.png'
+import techBlog from '../assets/Projects/Tech-blog.png'
